8abd38877b826b7f0b4e405fff682b58c45e8491
Lorenzo Tunesi <lorenzo.tunesi@polito.it>
2025-05-29 11:22:43 +0200

    added circular heater - Still to fix overlap

--- ring_resonators_v3.py
+++ ring_resonators_v3.py
@@ -50,105 +50,101 @@ c.add_port("through", port=r2.ports["o4"])
 c.add_port("add", port=r2.ports["o2"])
 
 
-# ====================
-# Micro heaters
-# ====================
-# clockwise numerated, starting from bottom left
-# sovrapposizione minima con il routing: 5
-
-# def add_heater(name_prefix, x_offset, y_offset, height, orientation):
-#     if (orientation == "vertical"):
-#         h = c << gf.components.rectangle(size=(heater_width, height), layer=LAYER_HEATER)
-#     elif (orientation == "orizzontal"):
-#         h = c << gf.components.rectangle(size=(height, heater_width), layer=LAYER_HEATER)
-#     h.movex(x_offset)
-#     h.movey(y_offset)
-#     if (orientation == "vertical"):
-#         port1 = c << gf.components.rectangle(size=(heater_width, heater_width), layer=LAYER_HEATER)
-#         port1.movex(x_offset)
-#         port1.movey(y_offset)
-#         port2 = c << gf.components.rectangle(size=(heater_width, heater_width), layer=LAYER_HEATER)
-#         port2.movex(x_offset)
-#         port2.movey(y_offset-heater_width+height)
-#         c.add_port(f"{name_prefix}_1", port=port1.ports["e1"])
-#         c.add_port(f"{name_prefix}_2", port=port2.ports["e1"])
-#     elif (orientation == "orizzontal"):
-#         port1 = c << gf.components.rectangle(size=(heater_width, heater_width), layer=LAYER_HEATER)
-#         port1.movex(heater_width)
-#         port2 = c << gf.components.rectangle(size=(heater_width, heater_width), layer=LAYER_HEATER)
-#         port2.movex(-heater_width)
-#         c.add_port(f"{name_prefix}_1", port=port1.ports["e4"])
-#         c.add_port(f"{name_prefix}_2", port=port2.ports["e4"])
-    
-#     return h
-
-# add_heater("h1", - (coupling_length + 2 * rad1[0] + heater_width / 2),
-#            rad1[0] + guide_width + gaps1[0], 
-#            vertical_length[0], "vertical")
-# add_heater("h2", - (coupling_length + 2 * rad1[0] + heater_width / 2),
-#            2 * rad1[0] + rad1[1] + 2 * guide_width + gaps1[0] + gaps1[1] + vertical_length[0],
-#            vertical_length[1], "vertical")
-
-# h3 = c << gf.components.rectangle(size=(distance_between_rings, heater_width), layer=(2, 0))
-# h3.connect("e1", r1.ports["o1"], allow_width_mismatch = True, allow_layer_mismatch = True, allow_type_mismatch=True) 
-# c.add_port("h3_1", center=((distance_between_rings), (2*rad1[0] + 2*rad1[1] + gaps1[0] + gaps1[1] + gaps1[2] + 4*guide_width + vertical_length[0] + vertical_length[1])), orientation=90, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h3_2", center=(0, (2*rad1[0] + 2*rad1[1] + 3*guide_width + gaps1[0] + gaps1[1] + gaps1[2] + vertical_length[0] + vertical_length[1])), orientation=90, port_type="electrical", cross_section="metal_routing")
-
-
-# add_heater("h4", coupling_length + 2 * rad2[1] - heater_width / 2 + distance_between_rings,
-#            2 * rad2[0] + rad2[1] + 2 * guide_width + gaps2[0] + gaps2[1] + vertical_length[0],
-#            vertical_length[1], "vertical")
-
-# add_heater("h5", coupling_length + 2 * rad2[0] - heater_width / 2 + distance_between_rings,
-#            rad2[0] + guide_width + gaps2[0],
-#            vertical_length[0], "vertical")
 contacts_east = [] 
 contacts_west = []
 contacts_north = []
 
-h1 = c << gf.components.rectangle(size=(heater_width, vertical_length[0]), layer=(2, 0))
-h1.movex(- (coupling_length + 2*rad1[0] + heater_width/2))
-h1.movey( rad1[0] + guide_width + gaps1[0]) 
+
+# ----------------------------------------------------------------------------
+# CODICE PER CREARE OGNI HEATER CIRCOLARE: 
+# (HEATER 1)
+
+xs_metal = gf.cross_section.heater_metal(width=heater_width, layer=LAYER_HEATER)
+b = gf.components.bend_circular(width=heater_width, radius=rad1[0], cross_section=xs_metal)
+sh = gf.components.straight(width=heater_width, length=coupling_length, cross_section=xs_metal)
+sv = gf.components.straight(width=heater_width, length=vertical_length[0], cross_section=xs_metal)
+
+bh1 = c.add_ref(b)
+bh1.rotate(-90)
+bh1.movex(- (coupling_length + 2*rad1[0]))
+bh1.movey( rad1[0] + guide_width + gaps1[0]) 
+
+bh2 = c.add_ref(b)
+bh3 = c.add_ref(b)
+bh4 = c.add_ref(b)
+sh1 = c.add_ref(sh)
+sh2 = c.add_ref(sv)
+sh3 = c.add_ref(sh)
+
+
+sh1.connect("e1", bh1.ports["e2"])
+bh2.connect("e1", sh1.ports["e2"])
+sh2.connect("e1", bh2.ports["e2"])
+bh3.connect("e1", sh2.ports["e2"])
+sh3.connect("e1", bh3.ports["e2"])
+bh4.connect("e1", sh3.ports["e2"])
+
+c.add_port("ch1_1", port=bh1.ports["e1"])
+c.add_port("ch1_2", port=bh4.ports["e2"])
 
 # Add top square
 s1_1 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s1_1.connect("e4",h1["e2"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s1_1.connect("e4",c["ch1_1"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s1_1.move([-(10-heater_width)/2,-10])
-# s1_1.movex(- (coupling_length + 2*rad1[0] + heater_width/2)- 10  + heater_width)
-# s1_1.movey( rad1[0] + guide_width + gaps1[0])
 
 # Add bottom square
 s1_2 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s1_2.connect("e2",h1["e4"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s1_2.connect("e2",c["ch1_2"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s1_2.move([-(10-heater_width)/2,+10])
 
 contacts_west.append(s1_1["e1"])
 contacts_west.append(s1_2["e1"])
 
-#c.add_port(port=s1_2["e1"],name="h1_1")
-# c.add_port("h1_1", center=(-(coupling_length + 2*rad1[0] -heater_width/2), (rad1[0] + guide_width + gaps1[0] + vertical_length[0])), orientation=180, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h1_2", center=(-(coupling_length + 2*rad1[0] -heater_width/2), (rad1[0] + guide_width + gaps1[0])), orientation=180, port_type="electrical", cross_section="metal_routing")
+# FINE CODICE PER HEATER CIRCOLARE
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# (HEATER 2)
+
+bh1_2 = c.add_ref(b)
+bh1_2.rotate(-90)
+bh1_2.movex(- (coupling_length + 2*rad1[0]))
+bh1_2.movey(  2*rad1[0] + rad1[1] + 2*guide_width + gaps1[0] +gaps1[1] + vertical_length[0]) 
+
+bh2_2 = c.add_ref(b)
+bh3_2 = c.add_ref(b)
+bh4_2 = c.add_ref(b)
+sh1_2 = c.add_ref(sh)
+sh2_2 = c.add_ref(sv)
+sh3_2 = c.add_ref(sh)
 
 
-h2 = c << gf.components.rectangle(size=(heater_width, vertical_length[0]), layer=(2, 0))
-h2.movex(- (coupling_length + 2*rad1[0] + heater_width/2))
-h2.movey( 2*rad1[0] + rad1[1] + 2*guide_width + gaps1[0] +gaps1[1] + vertical_length[0]) 
+sh1_2.connect("e1", bh1_2.ports["e2"])
+bh2_2.connect("e1", sh1_2.ports["e2"])
+sh2_2.connect("e1", bh2_2.ports["e2"])
+bh3_2.connect("e1", sh2_2.ports["e2"])
+sh3_2.connect("e1", bh3_2.ports["e2"])
+bh4_2.connect("e1", sh3_2.ports["e2"])
+
+c.add_port("ch2_1", port=bh1_2.ports["e1"])
+c.add_port("ch2_2", port=bh4_2.ports["e2"])
 
 # Same procedure for heater 2
 s2_1 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s2_1.connect("e4",h2["e2"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s2_1.connect("e4",c["ch2_1"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s2_1.move([-(10-heater_width)/2,-10])
 s2_2 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s2_2.connect("e2",h2["e4"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s2_2.connect("e2",c["ch2_2"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s2_2.move([-(10-heater_width)/2,+10])
 
 contacts_west.append(s2_1["e1"])
 contacts_west.append(s2_2["e1"])
 
-# c.add_port("h2_2", center=(-(coupling_length + 2*rad1[0] -heater_width/2), (2*rad1[0] + rad1[1] + 2*guide_width + gaps1[0] +gaps1[1] + vertical_length[0] + vertical_length[1])), orientation=180, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h2_1", center=(-(coupling_length + 2*rad1[0] -heater_width/2), (2*rad1[0] + rad1[1] + 2*guide_width + gaps1[0] +gaps1[1] + vertical_length[0])), orientation=180, port_type="electrical", cross_section="metal_routing")
 
-h3 = c << gf.components.rectangle(size=(distance_between_rings, heater_width), layer=(2, 0))
+# ----------------------------------------------------------------------------
+# (HEATER 3)
+
+h3 = c << gf.components.rectangle(size=(distance_between_rings, heater_width), layer=LAYER_HEATER)
 h3.connect("e1", r1.ports["o1"], allow_width_mismatch = True, allow_layer_mismatch = True, allow_type_mismatch=True) 
 
 s3_1 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
@@ -161,44 +157,95 @@ s3_2.move([-10, (10-heater_width)/2])
 contacts_north.append(s3_1["e3"])
 contacts_north.append(s3_2["e3"])
 
-# c.add_port("h3_1", center=((distance_between_rings), (2*rad1[0] + 2*rad1[1] + gaps1[0] + gaps1[1] + gaps1[2] + 3*guide_width + vertical_length[0] + vertical_length[1] -heater_width/2)), orientation=90, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h3_2", center=(0, (2*rad1[0] + 2*rad1[1] + gaps1[0] + gaps1[1] + gaps1[2] + 3*guide_width + vertical_length[0] + vertical_length[1] -heater_width/2)), orientation=90, port_type="electrical", cross_section="metal_routing")
 
-h4 = c << gf.components.rectangle(size=(heater_width, vertical_length[0]), layer=(2, 0))
-h4.movex( coupling_length + 2*rad2[1] - heater_width/2 + distance_between_rings)
-h4.movey( 2*rad2[0] + rad2[1] + 2*guide_width + gaps2[0] + gaps2[1] + vertical_length[0]) 
+# ----------------------------------------------------------------------------
+# (HEATER 4)
 
+bh1_3 = c.add_ref(b)
+bh1_3.rotate(-90)
+bh1_3.mirror()
+bh1_3.movex(coupling_length + 2*rad2[1] + distance_between_rings - heater_width)
+bh1_3.movey(2*rad2[0] + rad2[1] + 2*guide_width + gaps2[0] + gaps2[1] + vertical_length[0]) 
+
+bh2_3 = c.add_ref(b)
+bh2_3.mirror()
+bh3_3 = c.add_ref(b)
+bh3_3.mirror()
+bh4_3 = c.add_ref(b)
+bh4_3.mirror()
+sh1_3 = c.add_ref(sh)
+sh2_3 = c.add_ref(sv)
+sh3_3 = c.add_ref(sh)
+
+
+sh1_3.connect("e1", bh1_3.ports["e2"])
+bh2_3.connect("e1", sh1_3.ports["e2"])
+sh2_3.connect("e1", bh2_3.ports["e2"])
+bh3_3.connect("e1", sh2_3.ports["e2"])
+sh3_3.connect("e1", bh3_3.ports["e2"])
+bh4_3.connect("e1", sh3_3.ports["e2"])
+
+c.add_port("ch4_1", port=bh1_3.ports["e1"])
+c.add_port("ch4_2", port=bh4_3.ports["e2"])
+
+# Same procedure for heater 4
 s4_1 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s4_1.connect("e4",h4["e2"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s4_1.connect("e4",c["ch4_1"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s4_1.move([(10-heater_width)/2,-10])
 s4_2 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s4_2.connect("e2",h4["e4"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s4_2.connect("e2",c["ch4_2"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s4_2.move([(10-heater_width)/2,+10])
 
 contacts_east.append(s4_1["e3"])
 contacts_east.append(s4_2["e3"])
 
 
-# c.add_port("h4_2", center=((coupling_length + 2*rad1[0] -heater_width/2 + distance_between_rings), (2*rad2[0] + rad2[1] + 2*guide_width + gaps2[0] +gaps2[1] + vertical_length[0] + vertical_length[1])), orientation=0, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h4_1", center=((coupling_length + 2*rad1[0] -heater_width/2 + distance_between_rings), (2*rad2[0] + rad2[1] + 2*guide_width + gaps2[0] +gaps2[1] + vertical_length[0])), orientation=0, port_type="electrical", cross_section="metal_routing")
+# ----------------------------------------------------------------------------
+# (HEATER 5)
 
-h5 = c << gf.components.rectangle(size=(heater_width, vertical_length[0]), layer=(2, 0))
-h5.movex( coupling_length + 2*rad2[0] - heater_width/2 + distance_between_rings)
-h5.movey( rad2[0] + guide_width + gaps2[0]) 
+bh1_4 = c.add_ref(b)
+bh1_4.rotate(-90)
+bh1_4.mirror()
+bh1_4.movex(coupling_length + 2*rad2[0] - heater_width + distance_between_rings)
+bh1_4.movey(rad2[0] + guide_width + gaps2[0]) 
+
+bh2_4 = c.add_ref(b)
+bh2_4.mirror()
+bh3_4 = c.add_ref(b)
+bh3_4.mirror()
+bh4_4 = c.add_ref(b)
+bh4_4.mirror()
+sh1_4 = c.add_ref(sh)
+sh2_4 = c.add_ref(sv)
+sh3_4 = c.add_ref(sh)
+
+
+sh1_4.connect("e1", bh1_4.ports["e2"])
+bh2_4.connect("e1", sh1_4.ports["e2"])
+sh2_4.connect("e1", bh2_4.ports["e2"])
+bh3_4.connect("e1", sh2_4.ports["e2"])
+sh3_4.connect("e1", bh3_4.ports["e2"])
+bh4_4.connect("e1", sh3_4.ports["e2"])
+
+c.add_port("ch5_1", port=bh1_4.ports["e1"])
+c.add_port("ch5_2", port=bh4_4.ports["e2"])
 
 s5_1 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s5_1.connect("e4",h5["e2"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s5_1.connect("e4",c["ch5_1"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s5_1.move([(10-heater_width)/2,-10])
 s5_2 = c << gf.components.rectangle(size=(10, 10), layer=(12, 0))
-s5_2.connect("e2",h5["e4"],allow_width_mismatch=True,allow_layer_mismatch=True)
+s5_2.connect("e2",c["ch5_2"],allow_width_mismatch=True,allow_layer_mismatch=True)
 s5_2.move([(10-heater_width)/2,+10])
 
 contacts_east.append(s5_1["e3"])
 contacts_east.append(s5_2["e3"])
 
 
-# c.add_port("h5_1", center=((coupling_length + 2*rad1[0] -heater_width/2 + distance_between_rings), (rad2[0] + guide_width + gaps2[0] + vertical_length[0])), orientation=0, port_type="electrical", cross_section="metal_routing")
-# c.add_port("h5_2", center=((coupling_length + 2*rad1[0] -heater_width/2 + distance_between_rings), (rad2[0] + guide_width + gaps2[0])), orientation=0, port_type="electrical", cross_section="metal_routing")
+
+
+h1 = c << gf.components.rectangle(size=(heater_width, vertical_length[0]), layer=(2, 0))
+h1.movex(- (coupling_length + 2*rad1[0] + heater_width/2))
+h1.movey( rad1[0] + guide_width + gaps1[0]) 
 
 
 # ========================
@@ -216,8 +263,6 @@ pad_array_ex = c << gf.components.pad_array("pad", columns=num_pads, column_pitc
 for pad_array in (pad_array_in, pad_array_ex):
     pad_array.movex(-num_pads / 2 * pad_spacing + pad_size / 2 )
     pad_array.movey(distance_optic_pad)
-
-
 
 # ====================
 # Electrical Routing
@@ -290,5 +335,5 @@ routes = gf.routing.route_bundle(
 )
 
 
-c.write_gds(f"ring_resonatorsRR.gds")
+c.write_gds(f"ring_resonators.gds")
 
